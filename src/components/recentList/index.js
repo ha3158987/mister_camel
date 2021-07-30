0aecea532f44a0aeafeb7f1094459b32c09afb56
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-07-31 00:54:11 +0900

    feat: Input 버튼 onChange event 연결 (#38)

--- src/components/recentList/index.js
+++ src/components/recentList/index.js
@@ -1,3 +1,5 @@
+export { default as Filter } from './Filter';
 export { default as FilterBtns } from './FilterBtns';
 export { default as FilterPopup } from './FilterPopup';
 export { default as InputGroup } from './InputGroup';
+export { default as RecentProductList } from './RecentProductList';
